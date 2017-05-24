88273b9153760c0f3feb0f1cd09d3661f3dc82cf
kadoyau <el.psi.congroo@gmail.com>
2017-05-25 02:45:09 +0900

    クラスの作成
    yarn startで次の文字列が出力
    Wah wah, I am Toby

---
+++ src/dog.js
@@ -0,0 +1,12 @@
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+
+  bark() {
+    return `Wah wah, I am ${this.name}`;
+  }
+}
+
+// このクラスを外部ファイルからrequireして利用するために必要
+module.exports = Dog;
