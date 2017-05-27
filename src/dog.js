915b708edc8538454246fe307f376b83104ff4ae
kadoyau <el.psi.congroo@gmail.com>
2017-05-28 05:12:59 +0900

    ESLintに怒られないようにする

--- src/dog.js
+++ src/dog.js
@@ -1,13 +1,13 @@
 class Dog {
   constructor(name) {
-    this.name = name;
+    this.name = name
   }
 
   bark() {
-    return `Wah wah, I am ${this.name}`;
+    return `Wah wah, I am ${this.name}`
   }
 }
 
 // このクラスを外部ファイルからrequireして利用するために必要
 // module.exports = Dog; と等価
-export default Dog;
+export default Dog
